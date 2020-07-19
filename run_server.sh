ab5e698bda4d2be4f946e53f55f7f3a7f2bad7ab
zalmane <oren.elias@gmail.com>
2020-07-19 11:34:11 +0300

    cleanup

--- run_server.sh
+++
@@ -1,2 +0,0 @@
-source venv/bin/activate
-jupyter server --ServerApp.token=nstjupyter --port 9007 --ServerApp.allow_remote_access=True --MappingKernelManager.default_kernel_name=venv --KernelManager.kernel_name=venv --ServerApp.open_browser=False
